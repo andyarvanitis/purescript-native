98493403bc512c1ad0387e63072c05eddb994de5
Andy Arvanitis <andyarvanitis@gmail.com>
2015-05-02 19:41:50 -0700

    Switch to C++14 compiler flag

--- src/Language/PureScript/Cpp.hs
+++ src/Language/PureScript/Cpp.hs
@@ -321,5 +321,5 @@ cmakeListsTxt = intercalate "\n" lines'
                  , "file (GLOB_RECURSE HDRS *.hh)"
                  , "add_executable (Main ${SRCS} ${HDRS})"
                  , "include_directories (${CMAKE_CURRENT_SOURCE_DIR})"
-                 , "set (CMAKE_CXX_FLAGS ${CMAKE_CXX_FLAGS} \"-std=c++11\")"
+                 , "set (CMAKE_CXX_FLAGS ${CMAKE_CXX_FLAGS} \"-std=c++14\")"
                  ]
