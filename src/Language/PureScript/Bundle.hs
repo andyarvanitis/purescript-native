purescript/purescript

commit ae98dfafa9a20f93e26f1fc0e6bca813f50ea1f8

Exhaustive pattern match on JSAnnot. (#2432)
* Exhaustive pattern match on JSAnnot.

Also append to contributors file.

* Bump LTS versions for stack.

src/Language/PureScript/Bundle.hs
@@ -534,6 +534,7 @@ codeGen optionsMainModule optionsNamespace ms =  renderToString (JSAstProgram (p
     where
     squash JSNoAnnot = JSAnnot (TokenPn 0 0 2) []
     squash (JSAnnot pos ann) = JSAnnot (keepCol pos) (map splat ann)
+    squash JSAnnotSpace = JSAnnot (TokenPn 0 0 2) []
 
     splat (CommentA pos s) = CommentA (keepCol pos) s
     splat (WhiteSpace pos w) = WhiteSpace (keepCol pos) w
@@ -589,6 +590,7 @@ codeGen optionsMainModule optionsNamespace ms =  renderToString (JSAstProgram (p
 
       appendAnn a JSNoAnnot = JSAnnot tokenPosnEmpty [a]
       appendAnn a (JSAnnot _ anns) = JSAnnot tokenPosnEmpty (a:anns ++ [WhiteSpace tokenPosnEmpty "  "])
+      appendAnn a JSAnnotSpace = JSAnnot tokenPosnEmpty [a]
 
   runMain :: String -> [JSStatement]
   runMain mn =

stack-ghc-8.0.yaml
@@ -1,4 +1,4 @@
-resolver: nightly-2016-07-19
+resolver: lts-7.9
 packages:
 - '.'
 extra-deps:

stack.yaml
@@ -1,3 +1,3 @@
-resolver: lts-6.10
+resolver: lts-6.25
 packages:
 - '.'

andyarvanitis/purescript-native

commit fa810c9e9a9b7a65965f4784b69a74ce0ef92713

Fixed compile broken by commit ae98dfafa9

src/Language/PureScript/Bundle.hs
@@ -534,7 +534,6 @@ codeGen optionsMainModule optionsNamespace ms =  renderToString (JSAstProgram (p
     where
     squash JSNoAnnot = JSAnnot (TokenPn 0 0 2) []
     squash (JSAnnot pos ann) = JSAnnot (keepCol pos) (map splat ann)
-    squash JSAnnotSpace = JSAnnot (TokenPn 0 0 2) []
 
     splat (CommentA pos s) = CommentA (keepCol pos) s
     splat (WhiteSpace pos w) = WhiteSpace (keepCol pos) w
@@ -590,7 +589,6 @@ codeGen optionsMainModule optionsNamespace ms =  renderToString (JSAstProgram (p
 
       appendAnn a JSNoAnnot = JSAnnot tokenPosnEmpty [a]
       appendAnn a (JSAnnot _ anns) = JSAnnot tokenPosnEmpty (a:anns ++ [WhiteSpace tokenPosnEmpty "  "])
-      appendAnn a JSAnnotSpace = JSAnnot tokenPosnEmpty [a]
 
   runMain :: String -> [JSStatement]
   runMain mn =