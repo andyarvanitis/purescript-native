aea8df3aeb44ef2bf7758aba1082dade02b28ca8
Andy Arvanitis <andyarvanitis@gmail.com>
2019-08-25 17:21:30 -0700

    Add more default ffi packages

--- support/ffi-loader.go
+++ support/ffi-loader.go
@@ -8,14 +8,19 @@ import (
 	_ "github.com/purescript-native/go-ffi/purescript-assert"
 	_ "github.com/purescript-native/go-ffi/purescript-console"
 	_ "github.com/purescript-native/go-ffi/purescript-effect"
+	_ "github.com/purescript-native/go-ffi/purescript-exceptions"
 	_ "github.com/purescript-native/go-ffi/purescript-foldable-traversable"
 	_ "github.com/purescript-native/go-ffi/purescript-functions"
+	_ "github.com/purescript-native/go-ffi/purescript-globals"
+	_ "github.com/purescript-native/go-ffi/purescript-integers"
+	_ "github.com/purescript-native/go-ffi/purescript-math"
 	_ "github.com/purescript-native/go-ffi/purescript-partial"
 	_ "github.com/purescript-native/go-ffi/purescript-prelude"
 	_ "github.com/purescript-native/go-ffi/purescript-random"
 	_ "github.com/purescript-native/go-ffi/purescript-refs"
 	_ "github.com/purescript-native/go-ffi/purescript-st"
 	_ "github.com/purescript-native/go-ffi/purescript-strings"
+	_ "github.com/purescript-native/go-ffi/purescript-unfoldable"
 )
 
 // Add your own FFI packages here.
