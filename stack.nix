08492aec47ff185d4bc9782f1e3bfc3890d488a6
Brandon Elam Barker <beb82@cornell.edu>
2019-10-12 03:18:51 +0000

    fix locale and add ncurses in nix

--- stack.nix
+++ stack.nix
@@ -1,8 +1,12 @@
+#
+# You may need to set LC_ALL when building, e.g.
+# LC_ALL=en_US.iso88591 stack --nix build
+#
 { pkgs ? import <nixpkgs> {}, ghc ? pkgs.ghc }:
 
 pkgs.haskell.lib.buildStackProject {
   name = "default-stack-shell";
   inherit ghc;
-  buildInputs = with pkgs; [git git-lfs gmp zlib];
-  LANG = "en_US.UTF-8";
+  buildInputs = with pkgs; [git git-lfs gmp ncurses zlib];
+  LANG = "en_US.iso88591";
 }
