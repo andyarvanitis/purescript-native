c208363e9348c17a9b57612e1f05a69d0df04b50
Brandon Elam Barker <beb82@cornell.edu>
2019-10-14 20:03:50 +0000

    nix build: revert LANG; add other gotchas header comments

--- stack.nix
+++ stack.nix
@@ -1,6 +1,11 @@
 #
+# You may need to manually run `unset STACK_IN_NIX_SHELL`
+# (doesn't work from a `shellHook`; see: https://github.com/commercialhaskell/stack/issues/5000
+#
+#
 # You may need to set LC_ALL when building, e.g.
-# LC_ALL=en_US.iso88591 stack --nix build
+# LC_ALL=en_US.iso88591 stack --nix build; see e.g. https://github.com/koalaman/shellcheck/issues/324
+# 
 #
 { pkgs ? import <nixpkgs> {}, ghc ? pkgs.ghc }:
 
@@ -8,5 +13,5 @@ pkgs.haskell.lib.buildStackProject {
   name = "default-stack-shell";
   inherit ghc;
   buildInputs = with pkgs; [git git-lfs gmp ncurses zlib];
-  LANG = "en_US.iso88591";
+  LANG = "en_US.UTF-8";
 }
